ec0dd255376ce3d56ed473a3ac73c229cdc39e3d
kumar11jr <kumar11jr@gmail.com>
2023-12-17 23:28:19 +0530

    updating schema

---
+++ apps/server/src/db/schema.ts
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const dataSchema = new Schema({
+    url:{type: String,required:true},
+    hashcode : {type: String,required:true}
+})
+
+
+const data = mongoose.model('data', dataSchema)
+
+export default data
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,28 @@
+import { Collection } from 'mongoose';
+import run from './db/conn'
+import data from './db/schema';
 const express = require('express');
 
 const app = express();
 
 app.get('/',(req:any, res:any) => {
-  res.send('Hello Prabhat lodu');
+  run()
+  res.send("hijkhuygtf")
+})
+
+
+app.get('/api/data',async(req:any,res:any)=>{
+  try {
+    run(); // Assuming this connects to MongoDB (run your connection logic)
+    
+    // Assuming "Collection" is your Mongoose model
+    data.insertOne({url:"www.google.com",hashcode:"hdsdgvsdglg"});
+    console.log("insett")
+    // Retrieve all data from the collection
+    // res.json(data); // Send retrieved data as JSON
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
 })
 
 app.listen(8080);
