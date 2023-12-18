7c47bb2fd96f4eb35966df8736801b35228bacee
AdityaSingh-02 <asaditya2002@gmail.com>
2023-12-18 17:50:51 +0530

    fix: Db connection

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,28 +5,27 @@ const { connectDB } = require('./db/conn');
 import {TypedRequestBody} from './types/ITypedReq'
 
 const app = Express();
-
+app.use(Express.json());
 connectDB();
 
 app.get('/', (_req: Express.Request, res: Express.Response) => {
   res.status(200).send("OK");
 });
 
-app.get('/api/data', async (_req: TypedRequestBody<{url: string, hashcode: string}>, res: Express.Response) => {
-  const newData = new Shortner({
-    url: 'www.google.com',
-    hashcode: 'oggo.cm',
-  });
 
-  newData
-    .save()
-    .then((savedData: any) => {
-      console.log('Saved:', savedData);
-    })
-    .catch((error: any) => {
-      console.error('Error saving data:', error);
-    });
-  res.send('okiee');
+app.post('/api/data', async (req: TypedRequestBody<{url: string, hashcode: string}>, res: Express.Response) => {
+  try{
+    const userInput = {
+      url: req.body.url,
+      hashcode: req.body.hashcode
+    };
+  
+    const short = await Shortner.create(userInput);
+
+    res.status(201).send(short);
+  }catch(err:any){
+    res.status(400).send("Error")
+  }
 });
 
 app.listen(4000);
