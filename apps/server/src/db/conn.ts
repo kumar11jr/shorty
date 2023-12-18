2417af955b1b70c437ec30fe9ba0ab0ce0ea0413
kumar11jr <kumar11jr@gmail.com>
2023-12-18 10:58:18 +0530

    remove comments

--- apps/server/src/db/conn.ts
+++ apps/server/src/db/conn.ts
@@ -5,14 +5,6 @@ async function connect(){
   return mongoose.connect(`${process.env.DB_KEY}`)
 }
 
-// const DB = 'mongodb+srv://kumarshahil828:Randi123@cluster0.9ryvhye.mongodb.net/?retryWrites=true&w=majority';
-
-// mongoose.connect(DB).then(() => {
-//   console.log("Connected to MongoDB");
-// }).catch((err: any) => {
-//   console.error("Error connecting to MongoDB:", err);
-// });
-
 module.exports = {
   connect
 }
