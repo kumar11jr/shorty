22efb756813a38dd4cd026b14230ea613aee4f87
AdityaSingh-02 <asaditya2002@gmail.com>
2023-12-19 23:43:48 +0530

    update route

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,48 +3,56 @@ const { Shortner } = require('./db/schema');
 import Express from 'express';
 const { connectDB } = require('./db/conn');
 import { TypedRequestBody } from './types/ITypedReq';
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = Express();
 app.use(Express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
 connectDB();
 
 app.get('/', (_req: Express.Request, res: Express.Response) => {
-  res.status(200).send('OK');
+	res.status(200).json({ msg: 'Ok Success' });
 });
- 
+
 app.post('/api/data', async (req: TypedRequestBody<{ url: string; hashcode: string }>, res: Express.Response) => {
-  try {
-    // Inserts Data into the Db along with the Hashcode.
-    const userInput = {
-      url: req.body.url,
-      hashcode: req.body.hashcode,
-    };
-    const short = await Shortner.create(userInput);
+	try {
+		// Inserts Data into the Db along with the Hashcode.
+		const userInput = {
+			url: req.body.url,
+			hashcode: req.body.hashcode,
+		};
+		const short = await Shortner.create(userInput);
 
-    res.status(201).send(short);
-  } catch (err: any) {
-    res.status(400).send('Error');
-  }
+		res.status(201).send(short);
+	} catch (err: any) {
+		res.status(400).send('Error');
+	}
 });
 
-app.post('/redirect', async(req: TypedRequestBody<{url: string}>, res: Express.Response)=> {
-  try{
-     //returns The data according to the hashcode.
-    const url = await req.body.url;
-    const data = await Shortner.find({hashcode: url});
-    res.status(201).send(data);
-  }catch(err:any){
-    res.status(404).send("No data available")
-  }
-})
+app.post('/redirect', async (req: TypedRequestBody<{ hashcode: string }>, res: Express.Response) => {
+	try {
+		//returns The data according to the hashcode.
+		const url = await req.body.hashcode;
+		const data = await Shortner.find({ hashcode: url });
+		res.status(200).send(data);
+	} catch (err: any) {
+		res.status(404).send('No data available');
+	}
+});
 
 app.get('/all', async (req: Express.Request, res: Express.Response) => {
-  try {
-    const data = await Shortner.find({});
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(400).send('error');
-  }
+	try {
+		const data = await Shortner.find({});
+		res.status(200).send(data);
+	} catch (error) {
+		res.status(400).send('error');
+	}
 });
 
 app.listen(4000);
---
+++ apps/web/app/[id]/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+
+const page = () => {
+	const [data, setData] = useState('');
+	let path = usePathname();
+	path = path.substring(1);
+
+	useEffect(() => {
+		fetch('http://localhost:4000').then((res) => console.log(res));
+		// fetch('http://localhost:4000/redirect', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// 	body: path,
+		// })
+		// 	.then((res) => {
+		// 		res.json();
+		// 		console.log(res);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log('err', err);
+		// 	});
+	}, [path]);
+
+	return <>{path}</>;
+};
+
+export default page;
