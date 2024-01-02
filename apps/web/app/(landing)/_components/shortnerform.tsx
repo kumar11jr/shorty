74e5f04b94d75c59fcd23ef3bc54d9d2ad9d434b
AdityaSingh-02 <asaditya2002@gmail.com>
2024-01-02 12:07:51 +0530

    add: shadcn

--- apps/web/app/(landing)/_components/shortnerform.tsx
+++ apps/web/app/(landing)/_components/shortnerform.tsx
@@ -1,63 +1,129 @@
+// 'use client';
+// import React, { FormEvent, useState } from 'react';
+// import './Hero.css';
+// import { nanoid } from 'nanoid/non-secure';
+
+// const ShotnerForm = () => {
+// 	const [hashcode, setHashcode] = useState<string>('');
+// 	const [copied, setCopied] = useState<boolean>(false);
+
+// 	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+// 		event.preventDefault();
+// 		const inputValue = (event.target as HTMLFormElement).linkInput.value;
+// 		try {
+// 			const response = await fetch('http://localhost:4000/api/shortUrl', {
+// 				method: 'POST',
+// 				headers: {
+// 					'Content-Type': 'application/json',
+// 				},
+// 				body: JSON.stringify({ url: inputValue, hashcode: nanoid(8) }),
+// 			});
+// 			if (response.ok) {
+// 				const data = await response.json();
+// 				setHashcode(data.hashcode);
+// 				(event.target as HTMLFormElement).linkInput.value = '';
+// 			}
+// 		} catch (error) {
+// 			console.error('Error submitting URL:', error);
+// 		}
+// 	};
+
+// 	const handleCopy = () => {
+// 		navigator.clipboard.writeText(`http://localhost:3000/${hashcode}`).then((res) => {
+// 			setCopied(true);
+// 		});
+// 	};
+
+// 	return (
+// 		<>
+// 			<section className="hero-section">
+// 				<div className="container">
+// 					<h1 className="hero-label">Shorty</h1>
+// 					<form className="link-form" onSubmit={handleSubmit}>
+// 						<input type="text" id="linkInput" name="linkInput" placeholder="Paste your link here..." />
+// 						<button type="submit">Submit</button>
+// 					</form>
+// 					{hashcode && (
+// 						<div>
+// 							<p>
+// 								Shorty URL:{' '}
+// 								<a href={`http://localhost:3000/${hashcode}`} target="_blank" rel="noopener noreferrer">
+// 									http://localhost:3000/{hashcode}
+// 								</a>
+// 							</p>
+// 							<button onClick={handleCopy}>{copied ? 'copied' : 'copy url'}</button>
+// 						</div>
+// 					)}
+// 				</div>
+// 			</section>
+// 		</>
+// 	);
+// };
+
+// export default ShotnerForm;
+
 'use client';
-import React, { FormEvent, useState } from 'react';
-import './Hero.css';
-import { nanoid } from 'nanoid/non-secure';
 
-const ShotnerForm = () => {
-	const [hashcode, setHashcode] = useState<string>('');
-	const [copied, setCopied] = useState<boolean>(false);
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		const inputValue = (event.target as HTMLFormElement).linkInput.value;
-		try {
-			const response = await fetch('http://localhost:4000/api/shortUrl', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ url: inputValue, hashcode: nanoid(8) }),
-			});
-			if (response.ok) {
-				const data = await response.json();
-				setHashcode(data.hashcode);
-				(event.target as HTMLFormElement).linkInput.value = '';
-			}
-		} catch (error) {
-			console.error('Error submitting URL:', error);
-		}
-	};
+import { Button } from '../../../components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '../../../components/ui/form';
+import { Input } from '../../../components/ui/input';
 
-	const handleCopy = () => {
-		navigator.clipboard.writeText(`http://localhost:3000/${hashcode}`).then((res) => {
-			setCopied(true);
-		});
-	};
+const formSchema = z.object({
+	username: z.string().min(2, {
+		message: 'Username must be at least 2 characters.',
+	}),
+});
+
+function ShortnerForm() {
+	// ...
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: '',
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
 
 	return (
-		<>
-			<section className="hero-section">
-				<div className="container">
-					<h1 className="hero-label">Shorty</h1>
-					<form className="link-form" onSubmit={handleSubmit}>
-						<input type="text" id="linkInput" name="linkInput" placeholder="Paste your link here..." />
-						<button type="submit">Submit</button>
-					</form>
-					{hashcode && (
-						<div>
-							<p>
-								Shorty URL:{' '}
-								<a href={`http://localhost:3000/${hashcode}`} target="_blank" rel="noopener noreferrer">
-									http://localhost:3000/{hashcode}
-								</a>
-							</p>
-							<button onClick={handleCopy}>{copied ? 'copied' : 'copy url'}</button>
-						</div>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="username"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Username</FormLabel>
+							<FormControl>
+								<Input placeholder="shadcn" {...field} />
+							</FormControl>
+							<FormDescription>This is your public display name.</FormDescription>
+							<FormMessage />
+						</FormItem>
 					)}
-				</div>
-			</section>
-		</>
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
 	);
-};
+}
 
-export default ShotnerForm;
+export default ShortnerForm;
--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -3,7 +3,16 @@ import ShortnerForm from './_components/shortnerform';
 export default function LandingPage() {
 	return (
 		<>
-			<ShortnerForm />
+			<div className="flex flex-col space-y-36 items-center text-white bg-black h-[100vh] w-[100%]">
+				<div className="text-6xl mt-10 font-bold">
+					<h1>Shorty</h1>
+				</div>
+				<div className="">
+					<div>
+						<ShortnerForm />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
